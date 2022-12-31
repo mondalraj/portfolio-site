f34b3835c760aa6b9c5f0f4c01f574137c134ec7
Rajib Mondal <mondal021002rajib@gmail.com>
2022-12-31 11:50:48 +0530

    project cleanup

--- components/layout/Menubar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Menubar = () => {
-  return <div className="w-full h-6 backdrop-blur-2xl bg-white/50">Menubar</div>
-}
-
-export default Menubar
--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import HomeLayout from '../layout/HomeLayout'
-
-const Home: NextPage = () => {
-  return <HomeLayout>Hello World</HomeLayout>
-}
-
-export default Home
