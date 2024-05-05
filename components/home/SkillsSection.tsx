5c09444bde551c952d55121fd0f36f38c1c01027
Rajib Mondal <mondal021002rajib@gmail.com>
2024-05-05 10:15:54 +0530

    fix(): added skills section

--- components/home/SkillsSection.tsx
+++ components/home/SkillsSection.tsx
@@ -1,5 +1,12 @@
+import { Box } from "@mantine/core";
+
 const SkillsSection = () => {
-  return <div>SkillsSection</div>;
+  return (
+    <Box>
+      Next, Typescript, Nodejs, MongoDB, Postgresql, mongodb, prisma, Tailwind
+      CSS, Material UI, Mantine UI, Nestjs, Framer Motion
+    </Box>
+  );
 };
 
 export default SkillsSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "../components/home/AboutSection";
 import EducationSection from "../components/home/EducationSection";
 import ProjectSection from "../components/home/ProjectSection";
+import SkillsSection from "../components/home/SkillsSection";
 import WorkSection from "../components/home/WorkSection";
 import Layout from "../components/layout/Layout";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <Layout currentPage="Home">
       <AboutSection />
+      <SkillsSection />
       <WorkSection />
       <ProjectSection />
       <EducationSection />
